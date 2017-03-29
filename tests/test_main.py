4c46e2178f6549c5f84b220d82c575a2955a69c2
Florian POURTUGAU <florian.pourtugau@hotmail.fr>
2017-03-29 11:22:57 +0200

    Added tests for cli functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -61,4 +61,28 @@ class MainTestCase(unittest.TestCase):
             file_content = f.read()
         subprocess.call(["rm /tmp/test_undeploy"], shell=True)
         self.assertEqual(file_content, "* * * * * echo Good > /tmp/buffer\n")
+
+    def test_deploy_argparse(self):
+        subprocess.call(["echo '* * * * * echo argparse > /tmp/buffer' > /tmp/youcrontab.tab"], shell=True)
+        subprocess.call(["python ../deploycron/cli_deploycron_file.py /tmp/youcrontab.tab"], shell=True)
+        subprocess.call(["crontab -l > /tmp/test_filename_argparse"], shell=True)
+        with open("/tmp/test_filename_argparse") as f:
+            file_content = f.read()
+        subprocess.call(["rm /tmp/youcrontab.tab"], shell=True)
+        subprocess.call(["rm /tmp/test_filename_argparse"], shell=True)
+        self.assertEqual(file_content, "* * * * * echo argparse > /tmp/buffer\n")
+
+    def test_undeploy_argparse(self):
+        deploycron(content="* * * * * echo Good > /tmp/buffer")
+        deploycron(content="* * * * * echo day > /tmp/buffer")
+        deploycron(content="* * * * * echo to > /tmp/buffer")
+        deploycron(content="* * * * * echo you > /tmp/buffer")
+        deploycron(content="* * * * * echo mate > /tmp/buffer")
+        subprocess.call(["python ../deploycron/cli_undeploycron_between.py '* * * * * echo day > /tmp/buffer' '* * * * * echo mate > /tmp/buffer'"], shell=True)
+        subprocess.call(["crontab -l > /tmp/test_undeploy_argparse"], shell=True)
+        with open("/tmp/test_undeploy_argparse") as f:
+            file_content = f.read()
+        subprocess.call(["rm /tmp/test_undeploy_argparse"], shell=True)
+        self.assertEqual(file_content, "* * * * * echo Good > /tmp/buffer\n")
+        
         
